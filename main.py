205519d8cc8b128b33beae0c3e0b1efccf06514b
imisi99 <isongrichard234@gmail.com>
2024-04-23 14:28:45 +0100

    Surfaces

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+import sys
 import pygame
 
 pygame.init()
 
-WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
-display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
-
+WINDOW_WIDTH, WINDOW_HEIGHT = 1680, 720
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption(title="Asteroid Shooter")
+surface = pygame.Surface((1060, 480))
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+            sys.exit()
 
-
+    display_surface.fill("red")
+    surface.fill("yellow")
+    display_surface.blit(surface, (20, 20))
     pygame.display.update()
