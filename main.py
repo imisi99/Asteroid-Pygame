2983737e1b1d61eb597f39ea54a85190ccfa8b57
imisi99 <isongrichard234@gmail.com>
2024-04-26 20:05:18 +0100

    Meteor Addition

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+from random import randint
 import pygame
 
 
@@ -7,6 +8,13 @@ def laser_update(laser_list, speed=300):
         rect.y -= round(speed * dt)
         if rect.bottom < 0:
             laser_list.remove(rect)
+
+
+def meteor_update(meteor_list, speed=400):
+    for rect in meteor_list:
+        rect.y += round(speed * dt)
+        if rect.top > WINDOW_HEIGHT:
+            meteor_list.remove(rect)
 
 
 def score_display():
@@ -43,6 +51,9 @@ ship_flip = pygame.transform.flip(ship_surf, False, True)
 laser = pygame.image.load("./graphics/laser.png").convert_alpha()
 laser_list = []
 
+meteor = pygame.image.load("./graphics/meteor.png").convert_alpha()
+meteor_list = []
+
 font = pygame.font.Font("./graphics/subatomic.ttf", 70)
 font_credit = pygame.font.Font("./graphics/subatomic.ttf", 20)
 
@@ -52,11 +63,14 @@ text_rect = text.get_rect(center=((WINDOW_WIDTH / 2), (WINDOW_HEIGHT / 2)))
 text_credit = font_credit.render("Created By : Imisioluwa Isong", True, (200, 200, 200))
 text_credit_rect = text_credit.get_rect(center=((WINDOW_WIDTH / 2), (WINDOW_HEIGHT / 2) + 100))
 
-text_credit1 = font_credit.render("Director : Ichigo Kurosaki", True, (200, 200, 200))
+text_credit1 = font_credit.render("Director : Amaechi Daniel", True, (200, 200, 200))
 text_credit1_rect = text_credit1.get_rect(center=((WINDOW_WIDTH / 2), (WINDOW_HEIGHT / 2) + 150))
 
 can_shoot = True
 shoot_time = False
+
+meteor_timer = pygame.event.custom_type()
+pygame.time.set_timer(meteor_timer, 500)
 
 while True:
 
@@ -72,6 +86,12 @@ while True:
             laser_list.append(laser_rect)
             can_shoot = False
             shoot_time = pygame.time.get_ticks()
+
+        if event.type == meteor_timer and text_credit1_rect.y <= -20:
+            x_pos = randint(100, WINDOW_WIDTH - 100)
+            y_pos = randint(-50, -30)
+            meteor_rect = meteor.get_rect(center=(x_pos, y_pos))
+            meteor_list.append(meteor_rect)
 
     display_surface.fill((255, 255, 255))
     display_surface.blit(background, (0, 0))
@@ -96,7 +116,8 @@ while True:
 
         laser_update(laser_list)
         score_display()
-        can_shoot = shoot_timer(can_shoot, 500)
+        can_shoot = shoot_timer(can_shoot, 300)
+        meteor_update(meteor_list)
 
         display_surface.blit(ship_surf, ship_rect)
         score_display()
@@ -104,5 +125,15 @@ while True:
         for rect in laser_list:
             display_surface.blit(laser, rect)
 
+        for rect in meteor_list:
+            display_surface.blit(meteor, rect)
+
+    # Make it so that the ship doesn't shoot until it is in the bottom of the screen display
+    # Congratulating player when he gets a particular high score
+    # if len(laser_list) == 5:
+    #     text_wow = font.render("Congratulations!", True, (200, 200, 200))
+    #     text_wow_rect = text_wow.get_rect(center=((WINDOW_WIDTH / 2), (WINDOW_HEIGHT / 2)))
+    #     display_surface.blit(text_wow, text_wow_rect)
+    #     pygame.draw.rect(display_surface, "red", text_wow_rect.inflate(50, 30), width=7, border_radius=5)
+
     pygame.display.update()
-    print(len(laser_list))
